18468a040666f084ce9fa95f1cddf128618addcc
philtd5000 <philiptaft9@gmail.com>
2018-05-11 10:37:51 -0700

    continued flexbox overhaul and other stylistic changes

--- README.md
+++ README.md
@@ -3,5 +3,10 @@ A first step toward building my web development brand
 
 # NOTES:
 
+* CLEAN-UP CSS
 * write about me section in place of lorem ipsum (approx 100 words)
-* add jquery hover features for submit button
+* sticky footer
+* margin/padding/font-size for contact form
+* add a "fade out" effect on gallery and nav-bar links (if possible)
+    + adjust mz-transition etc (and test cross-browswer)
+* review how-to readme link sent by Leeland
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,9 @@
 console.log('CONNECTED TO JS FILE');
 
 $('.img-overlay').on('mouseover', function(){
-    this.classList.add('opaque');
+    this.classList.add('pop-effect');
 });
 
 $('.img-overlay').on('mouseout', function(){
-    this.classList.remove('opaque');
+    this.classList.remove('pop-effect');
 });
-
-$('.nav-link').on('mouseover', function(){
-    this.classList.add('pop');
-});
-
-$('.nav-link').on('mouseout', function(){
-    this.classList.remove('pop');
-})
-
-
-// var link1 = document.querySelectorAll(".img-overlay");
-// console.log(links);
-
-// links.addEventListener("mouseover", function(){
-//     this.classList.add("opaque");
-// });
-
-// links.addEventListener("mouseout", function(){
-//     this.classList.remove("opaque");
-// });
